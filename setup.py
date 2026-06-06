18522312c732de52a0c2df4750c9b3d9909c7702
saint <ysudqfs@163.com>
2026-06-06 15:34:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='data-packer3',
-    version='2.0.1',
+    version='2.1.0',
     description='A Python library for packing and unpacking data between different container formats',
     long_description=long_description,
     long_description_content_type='text/markdown',
